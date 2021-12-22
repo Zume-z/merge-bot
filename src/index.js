aff3dcafafeb001d8de34c9695561e99d93dd817
Brendan Elton <brendan.elton@gmail.com>
2021-12-22 23:14:39 +1100

    update

--- src/index.js
+++ src/index.js
@@ -11,51 +11,17 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 
-const params = {}
-
-client.post('statuses/update', { status: 'tweet' }, function (error, tweet, response) {
-  if (!error) {
-    console.log(tweet)
-  } else {
-    console.log(error)
-  }
-})
-
-const tweet = async () => {
-  const [smallMass, largeMass, combinedMass, tokenIdSmall, tokenIdLarge, tokenSupply, massTotal] = await massUpdate(event)
-
-  console.log(`
-m(${smallMass}) #${tokenIdSmall} merged with m(${largeMass}) #${tokenIdLarge}
-m #${tokenIdLarge} new mass = ${combinedMass}
-${tokenSupply}/28,388 merge remain.
-${massTotal}/312729 mass remains.
-  `)
-}
-tweet()
-
 async function main() {
   const [smallMass, largeMass, combinedMass, tokenIdSmall, tokenIdLarge, tokenSupply, massTotal] = await massUpdate(event)
   const params = {
-    status: `
-    m(${smallMass}) #${tokenIdSmall} merged with m(${largeMass}) #${tokenIdLarge}
-    m #${tokenIdLarge} new mass = ${combinedMass}
-    ${tokenSupply}/28,388 merge remain.
-    ${massTotal}/312729 mass remains.
-      `,
+    status:
+      `m(${smallMass}) #${tokenIdSmall} merged with m(${largeMass}) #${tokenIdLarge}.` +
+      '\n \n' +
+      `${tokenSupply}/28990 merge remain.
+
+    https://opensea.io/assets/0xc3f8a0f5841abff777d3eefa5047e8d413a1c9ab/${tokenIdLarge}
+  `,
   }
-
-  await client.post('account/update_profile', params)
-  console.log('🎉 Success! Updated Twitter bio/location and website')
+  client.post('statuses/update', params).catch((err) => console.log(err))
 }
-
 main().catch((err) => console.log(err))
-
-// async function grabGithubData(): Promise<string> {
-//   const browser = await puppeteer.launch()
-//   const page = await browser.newPage()
-//   await page.goto('https://github.com/users/erikch/contributions?from=2021-01-01')
-//   let contribs = await page.$$eval('[data-count]', (val) => val.reduce((acc, val) => acc + +val.getAttribute('data-count')!, 0))
-//   const currentYear = format(new Date(), 'yyyy')
-//   await browser.close()
-//   return `${currentYear} Github Contributions: ${contribs}`
-// }
