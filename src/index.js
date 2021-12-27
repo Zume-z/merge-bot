67f8333607c6e8b322e7690086188257815b365b
Brendan Elton <brendan.elton@gmail.com>
2021-12-27 20:11:58 +1100

    deploy

--- src/index.js
+++ src/index.js
@@ -17,41 +17,44 @@ const client = new Twitter({
 })
 
 const onMassUpdate = async (_, __, ___, event) => {
-  const tokenSupply = await contract.totalSupply()
-  const [tokenIdSmall, tokenIdLarge, combinedMass] = event.args
-  const previousBlockNumber = event.blockNumber - 1
-  const smallMass = await contract.massOf(tokenIdSmall, { blockTag: previousBlockNumber })
-  const largeMass = combinedMass - smallMass
-  const getMergeSvgUrl = () => {
-    return axios
-      .get(`https://api.opensea.io/api/v1/asset/0xc3f8a0F5841aBFf777d3eefA5047e8D413a1C9AB/${tokenIdLarge}`)
-      .then((res) => res.data.image_url)
-  }
-  const mergeSvgUrl = await getMergeSvgUrl()
-  const imageAsSvgString = (await axios.get(mergeSvgUrl)).data
-  const imageAsPngBuffer = await sharp(Buffer.from(imageAsSvgString)).png().toBuffer()
-
-  client.post('media/upload', { media: imageAsPngBuffer }, async function (error, media, response) {
-    if (!error) {
-      const status = {
-        status:
-          `m(${smallMass}) #${tokenIdSmall} + m(${largeMass}) #${tokenIdLarge}. => m(${combinedMass}) #${tokenIdLarge}.` +
-          '\n' +
-          `${tokenSupply}/28990 merge remain.` +
-          '\n' +
-          `https://opensea.io/assets/0xc3f8a0f5841abff777d3eefa5047e8d413a1c9ab/${tokenIdLarge}
-      `,
-        media_ids: media.media_id_string,
-      }
-      client.post('statuses/update', status, function (error, tweet, response) {
-        if (!error) {
-          console.log('Successful Tweet')
-        } else if (error) {
-          console.log(error)
-        }
-      })
+  try {
+    const tokenSupply = await contract.totalSupply()
+    const [tokenIdSmall, tokenIdLarge, combinedMass] = event.args
+    const previousBlockNumber = event.blockNumber - 1
+    const smallMass = await contract.massOf(tokenIdSmall, { blockTag: previousBlockNumber })
+    const largeMass = combinedMass - smallMass
+    const getMergeSvgUrl = () => {
+      return axios
+        .get(`https://api.opensea.io/api/v1/asset/0xc3f8a0F5841aBFf777d3eefA5047e8D413a1C9AB/${tokenIdLarge}`)
+        .then((res) => res.data.image_url)
     }
-  })
-}
+    const mergeSvgUrl = await getMergeSvgUrl()
+    const imageAsSvgString = (await axios.get(mergeSvgUrl)).data
+    const imageAsPngBuffer = await sharp(Buffer.from(imageAsSvgString)).png().toBuffer()
 
+    client.post('media/upload', { media: imageAsPngBuffer }, async function (error, media, response) {
+      if (!error) {
+        const status = {
+          status:
+            `m(${smallMass}) #${tokenIdSmall} + m(${largeMass}) #${tokenIdLarge}. => m(${combinedMass}) #${tokenIdLarge}.` +
+            '\n' +
+            `${tokenSupply}/28990 merge remain.` +
+            '\n' +
+            `https://opensea.io/assets/0xc3f8a0f5841abff777d3eefa5047e8d413a1c9ab/${tokenIdLarge}
+      `,
+          media_ids: media.media_id_string,
+        }
+        client.post('statuses/update', status, function (error, tweet, response) {
+          if (!error) {
+            console.log('Successful Tweet')
+          } else if (error) {
+            console.log(error)
+          }
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
 contract.on('MassUpdate', onMassUpdate)
